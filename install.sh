f9bd6f81b4f38520cb44961d038f94235a62b5a4
lyle <ly1564994969@outlook.com>
2024-03-09 09:25:30 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#安装git
+sudo apt install git
+
+# 安装zsh oh-my-zsh 
+sudo apt install zsh zsh-syntax-highlighting zsh-autosuggestions autojump
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+#安装fzf fd ripgrep bat
+sudo apt install fzf fd-find ripgrep bat
+mkdir -p ~/.local/bin
+ln -s /usr/bin/batcat ~/.local/bin/bat
+ln -s $(which fdfind) ~/.local/bin/fd    
+
+#下载 .zshrc
+cd ~
+curl -O -L https://raw.githubusercontent.com/disloot/debian_conf/main/.zshrc
+source .zshrc
+
+
