eab57915ee3744bd53baa6a178679c5f2ea50cd3
lyle <ly1564994969@outlook.com>
2024-03-09 10:06:14 +0800

    Update and rename install.sh to install.zsh

--- install.zsh
+++ install.zsh
@@ -1,10 +1,6 @@
-#!/bin/bash
+#!/bin/zsh
 #安装git
 sudo apt install git curl
-
-# 安装zsh oh-my-zsh 
-sudo apt install zsh zsh-syntax-highlighting zsh-autosuggestions autojump
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 #安装fzf fd ripgrep bat
 sudo apt install fzf fd-find ripgrep bat
@@ -12,9 +8,13 @@ mkdir -p ~/.local/bin
 ln -s /usr/bin/batcat ~/.local/bin/bat
 ln -s $(which fdfind) ~/.local/bin/fd    
 
+# 安装zsh oh-my-zsh 
+sudo apt install zsh zsh-syntax-highlighting zsh-autosuggestions autojump
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 #下载 .zshrc
 cd ~
 curl -O -L https://raw.githubusercontent.com/disloot/debian_conf/main/.zshrc
-source .zshrc
+source ~/.zshrc
 
 
