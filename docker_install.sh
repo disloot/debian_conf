2cce6e53b3d669ccacff502e603174e90c788396
lyle <ly1564994969@outlook.com>
2024-03-11 12:46:12 +0800

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -6,14 +6,14 @@ apt install -y git curl
 
 #安装fzf fd ripgrep bat
 apt install -y fzf fd-find ripgrep bat zsh
-mkdir -p ~/.local/bin
-ln -s /usr/bin/batcat ~/.local/bin/bat
-ln -s $(which fdfind) ~/.local/bin/fd    
+mkdir -p root/.local/bin
+ln -s /usr/bin/batcat root/.local/bin/bat
+ln -s $(which fdfind) root/.local/bin/fd    
 
 
 
 #下载 .zshrc
-curl -o ~/.zshrc https://raw.githubusercontent.com/disloot/debian_conf/main/.zshrc /root/.zshrc
+curl -o root/.zshrc https://raw.githubusercontent.com/disloot/debian_conf/main/.zshrc /root/.zshrc
 
 # 安装zsh oh-my-zsh 
 apt install -y zsh zsh-syntax-highlighting zsh-autosuggestions autojump
