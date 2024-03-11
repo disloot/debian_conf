2fc287907ff2f809449decd451cf7da84174109c
lyle <ly1564994969@outlook.com>
2024-03-11 12:11:14 +0800

    Create docker_install.sh

---
+++ docker_install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/disloot/debian_conf/main/install.sh)"
+#安装git
+apt install git curl
+
+#安装fzf fd ripgrep bat
+apt install fzf fd-find ripgrep bat
+mkdir -p ~/.local/bin
+ln -s /usr/bin/batcat ~/.local/bin/bat
+ln -s $(which fdfind) ~/.local/bin/fd    
+
+
+
+#下载 .zshrc
+curl -O -L https://raw.githubusercontent.com/disloot/debian_conf/main/.zshrc
+
+# 安装zsh oh-my-zsh 
+apt install zsh zsh-syntax-highlighting zsh-autosuggestions autojump
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
