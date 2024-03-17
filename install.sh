1f8dd579cd82fcdaa502668467b4e16cf8717cdb
lyle <ly1564994969@outlook.com>
2024-03-17 18:19:29 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,4 +22,17 @@ curl -o ~/.zshrc https://raw.githubusercontent.com/disloot/debian_conf/main/.zsh
 sudo apt install zsh zsh-syntax-highlighting zsh-autosuggestions autojump
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+#安装 neovim lazy-vim
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt update
+sudo apt install neovim
 
+mv ~/.config/nvim{,.bak}
+mv ~/.local/share/nvim{,.bak}
+mv ~/.local/state/nvim{,.bak}
+mv ~/.cache/nvim{,.bak}
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git
+
+#gcc
+sudo apt install build-essential
