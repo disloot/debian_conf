8c716e26ebdbc376e023b7e5aa75ac908f950b32
lyle <ly1564994969@outlook.com>
2024-04-11 16:11:34 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 #sh -c "$(curl -fsSL https://raw.githubusercontent.com/disloot/debian_conf/main/install.sh)"
 #安装git
 sudo apt update
-sudo apt install git curl
+sudo apt install -y git curl
 git config --global user.email "ly1564994969@outlook.com" 
 git config --global user.name "ly"
 
 #安装fzf fd ripgrep bat
-sudo apt install fzf fd-find ripgrep bat
+sudo apt install -y fzf fd-find ripgrep bat
 mkdir -p ~/.local/bin
 ln -s /usr/bin/batcat ~/.local/bin/bat
 ln -s $(which fdfind) ~/.local/bin/fd    
@@ -17,7 +17,7 @@ ln -s $(which fdfind) ~/.local/bin/fd
 
 
 # 安装zsh oh-my-zsh 
-sudo apt install zsh zsh-syntax-highlighting zsh-autosuggestions autojump
+sudo apt install -y zsh zsh-syntax-highlighting zsh-autosuggestions autojump
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 #下载 .zshrc
@@ -27,7 +27,7 @@ curl -o ~/.zshrc https://raw.githubusercontent.com/disloot/debian_conf/main/.zsh
 #安装 neovim lazy-vim
 sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt update
-sudo apt install neovim
+sudo apt install -y neovim
 
 mv ~/.config/nvim{,.bak}
 mv ~/.local/share/nvim{,.bak}
@@ -37,4 +37,4 @@ git clone https://github.com/LazyVim/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
 
 #gcc
-sudo apt install build-essential
+sudo apt install -y build-essential
